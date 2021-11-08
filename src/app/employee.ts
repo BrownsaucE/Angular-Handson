687eb3db7ad764868af995b0574c572cc83a8c08
Bronson <44597266+BrownsaucE@users.noreply.github.com>
2021-11-08 10:12:06 +0800

    Add files via upload

---
+++ src/app/employee.ts
@@ -0,0 +1,12 @@
+import { Skill } from "./skill";
+import { Department } from "./department";
+
+export interface Employee{
+    id: number;
+    name: string;
+    salary: number;
+    permanent: boolean;
+    department: Department;
+    skill: Skill[];
+    dateOfBirth: Date;
+}
