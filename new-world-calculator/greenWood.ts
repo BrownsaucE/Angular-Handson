06e531b6cbf7f9dfab0106a2d89a50361a1831e7
Bronson <44597266+BrownsaucE@users.noreply.github.com>
2021-12-17 13:21:46 +0800

    Add files via upload

---
+++ new-world-calculator/greenWood.ts
@@ -0,0 +1,3 @@
+export interface GreenWood{
+    number:number;
+}
---
+++ new-world-calculator/wrydwood-planks.ts
@@ -0,0 +1,8 @@
+import { Lumber } from "./lumber";
+
+export interface WyrdwoodPlanks{
+    wyrdwood:number;
+    lumber:number;
+    sandpaper: number;
+    number:number;
+}
